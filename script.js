947b73bc6464edf1c4f3117eba2134fc0cfa36e8
SuvamHere <suvamtops10@gmail.com>
2026-06-03 13:54:32 +0545

    added cache DOM refrences for core view screens

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const screens = {
+    landing: document.getElementById('screen-landing'),
+    username: document.getElementById('screen-username'),
+    select: document.getElementById('screen-select'),
+};
