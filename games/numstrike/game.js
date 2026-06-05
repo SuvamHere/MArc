0683be86a36fbb3c5fc052db1ef86e185695b556
SuvamHere <suvamtops10@gmail.com>
2026-06-05 22:56:34 +0545

    added const of all the elements i will be using further in the program

--- games/numstrike/game.js
+++ games/numstrike/game.js
@@ -4,3 +4,17 @@ const canvas= document.getElementById('game-canvas');
 const ctx= canvas.getContext('2d');
 const input= document.getElementById('answer-input');
 
+const overlayStart = document.getElementById('overlay-start');
+const overlayGameover = document.getElementById(overlay-gameover);
+
+const gameUI = Document.getElementById('game-ui');
+
+const hudScore = document.getElementById('hud-score');
+const hudLevel = document.getElementById('hud-level');
+const hudStreak = document.getElementById('hud-streak');
+
+const finalscore = document.getElementById('final-score');
+const finalBest = document.getElementById('final-best');
+const finallevel = document.getElementById('final-level');
+const finalstreak = document.getElementById('final-streak');
+const newBestBanner = document.getElementById('new-best-banner');
